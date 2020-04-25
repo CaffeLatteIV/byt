2061a41c6443b9c8504ec84d049b882d388ec49c
frittata <64194462+frittata@users.noreply.github.com>
2020-04-25 17:21:08 +0200

    blublublu

--- website/bello/js/script.js
+++ website/bello/js/script.js
@@ -13,3 +13,20 @@ var animation1 = bodymovin.loadAnimation({
   autoplay: true,
   path: 'img/back/tot.json'
 })
+
+var index_cont_btn = 1;
+
+function switchBackground() {
+   if (index_cont_btn == 1) {
+      var cont = document.getElementById("cont-btn");
+      cont.className = "bg-orange";
+      index_cont_btn = 0;
+   }
+   else {
+      var cont = document.getElementById("cont-btn");
+      cont.className = "bg-orange";
+      index_cont_btn = 1;
+   }
+}
+
+setInterval(switchBackground(), 2000);
