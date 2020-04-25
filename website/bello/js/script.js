f2743d614db01048c9e3f2f2d8e34235e88b254d
frittata <64194462+frittata@users.noreply.github.com>
2020-04-25 13:55:00 +0200

    sfonzo animazione
    eee

--- website/bello/js/script.js
+++ website/bello/js/script.js
@@ -5,3 +5,11 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: 'img/logo/tot.json'
 })
+
+var animation1 = bodymovin.loadAnimation({
+  container: document.getElementById('back-movin'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'img/back/tot.json'
+})
