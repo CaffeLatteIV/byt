b6a3dece146cca072f79767ea72992305e851325
frittata <64194462+frittata@users.noreply.github.com>
2020-04-25 13:31:30 +0200

    h
    y

---
+++ website/prove/js/script.js
@@ -0,0 +1,3 @@
+//caricamento animazione logo
+var animation = bodymovin.loadAnimation({container: document.getElementById('logo-anim'),renderer: 'svg',loop: false,autoplay: true,path: 'animation/logo/tot.json'})
+var animation1 = bodymovin.loadAnimation({container: document.getElementById('back-anim'),renderer: 'svg',loop: false,autoplay: true,path: 'animation/back/back.json'})
