4e649986174408f9e3006640e86850ad60d5c870
frittata <64194462+frittata@users.noreply.github.com>
2020-04-28 11:35:04 +0200

    contatti

---
+++ website/bello/js/script_pages.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '../img/logo/tot.json'
+  })
+  
+  var animation1 = bodymovin.loadAnimation({
+    container: document.getElementById('back-movin'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '../img/back/tot.json'
+  })
